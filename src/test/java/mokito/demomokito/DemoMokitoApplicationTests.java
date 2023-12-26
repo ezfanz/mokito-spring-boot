72a572c1faafcfa3caa33246a03dbeb558a7d67e
irfan zulkefly <irfan.zulkefly@accenture.com>
2023-12-26 16:00:06 +0800

    first commit

---
+++ src/test/java/mokito/demomokito/DemoMokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package mokito.demomokito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMokitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
